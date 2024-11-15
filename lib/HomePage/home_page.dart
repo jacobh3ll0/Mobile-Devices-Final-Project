0cd4d2d7c19ab9c08934f5c7930179c6b5f69008
Ryan Warrener <ryan.warrener@ontariotechu.net>
2024-11-15 18:51:50 -0500

    Added gif support, added quote http request, added some text styles, very primitative background

--- lib/HomePage/home_page.dart
+++ lib/HomePage/home_page.dart
@@ -6,27 +6,59 @@ import 'package:geocoding/geocoding.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:md_final/global_widgets/build_bottom_app_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_animate/flutter_animate.dart';
+
+import 'package:gif/gif.dart';
+
+TextStyle quoteStyle() {
+  return TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    color: Colors.red,
+    shadows: [
+      Shadow(
+        blurRadius: 10.0,
+        color: Colors.black,
+        offset: Offset(2.0, 2.0),
+      ),
+    ],
+  );
+}
+
+TextStyle timeStyle() {
+  return TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    color: Colors.red,
+    shadows: [
+      Shadow(
+        blurRadius: 10.0,
+        color: Colors.black,
+        offset: Offset(2.0, 2.0),
+      ),
+    ],
+  );
+}
 
 String calcTOD(int time)
 {
   if (time >= 500 && time < 1159)
     {
-      return "Good Morning User!";
+      return "Good Morning User! $time";
     }
   else if (time >= 1200 && time < 1559)
     {
-      return "Good Afternoon User!";
+      return "Good Afternoon User! $time";
     }
   else if (time >= 1600 && time < 1959)
     {
-      return "Good Evening User!";
+      return "Good Evening User! $time";
     }
   else
     {
-      return "Good Night User!";
+      return "Good Night User! $time";
     }
 
 }
@@ -38,25 +70,19 @@ String getTime()
   final hour = dt.hour.toString().padLeft(2,'0');
   final min = dt.minute.toString().padLeft(2,'0');
   final inttime = int.parse('$hour$min');
-  getQuote();
   return calcTOD(inttime);
-
 }
 
-void getQuote() async
+Future<Map<String, dynamic>> getQuote() async
 {
   final url = Uri.parse('https://zenquotes.io/api/today/');
   final response = await http.get(url);
   String quoteData = response.body.trim();
   List<dynamic> parsedQuote = jsonDecode(quoteData);
   Map<String, dynamic> quoteObject = parsedQuote[0];
-  log(quoteData);
-  log(quoteObject['q']);
-  log(quoteObject['a']);
+  return quoteObject;
 
 }
-
-
 
 class HomePage extends StatefulWidget
 {
@@ -65,6 +91,14 @@ class HomePage extends StatefulWidget
 }
 class HomePageState extends State<HomePage>
 {
+
+  @override
+  void initState()
+  {
+    super.initState();
+
+  }
+
   // const HomePage({super.key});
   @override
   Widget build(BuildContext context)
@@ -75,34 +109,50 @@ class HomePageState extends State<HomePage>
     return Scaffold(
         appBar: AppBar(title: const Text("Home Page"), automaticallyImplyLeading: false),
         body:
-        Container(
-          decoration: const BoxDecoration(
-            // image: DecorationImage(
-            //     image: AssetImage("testimage.PNG"),
-            //     fit: BoxFit.cover),
-          ),
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(getTime()),
-                  Text("INSERT TOWN/CITY"),
+            Stack(
+              children: [
+                Positioned.fill(
+                  child: Image.asset('lib/HomePage/test.gif',
+                  fit: BoxFit.cover)),
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(''),
+              Text(getTime(), style: timeStyle()),
+                buildQuote()
+              ],
+            ),]
+            ),
 
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Text('Hello'),
-                ],
-              )
-            ],
-          ),
-        )
+
+    );
+  }
+
+
+ Widget buildQuote()
+  {
+    return FutureBuilder<Map<String, dynamic>>
+      (
+      future: getQuote(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else if (snapshot.hasData) {
+          final data = snapshot.data!;
+          return Text(
+            '"${data['q']}" — ${data['a']}',
+            textAlign: TextAlign.center,
+            style: quoteStyle(),
+          );
+        }
+        else
+        {
+          return Text('No data available');
+        }
+      },
     );
   }
 }
