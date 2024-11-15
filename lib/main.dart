4d658fe6b1e9ff4fc9230d4d5271ff59ba341c83
jacobh3ll0 <jacob.rempel@ontariotechu.net>
2024-11-14 21:07:17 -0500

    did all the suggested flutter fixes

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:md_final/global_widgets/build_bottom_app_bar.dart';
 import 'package:md_final/global_widgets/theme_related/theme_manager.dart';
 import 'global_widgets/database_model.dart';
-import 'global_widgets/database_model_user_prefs.dart';
 
 //Authentication Handlers
 import 'session_manager.dart';
@@ -28,36 +27,38 @@ void main() async
 {
   WidgetsFlutterBinding.ensureInitialized(); //Ensures flutter binding initialized
   await Firebase.initializeApp(); //Initialize the firebase
-  runApp(FitnessApp());
+  runApp(const FitnessApp());
 }
 
 class FitnessApp extends StatelessWidget {
+  const FitnessApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     //init database, this will stay open, you can access it by creating a variable like below
     //basically if it is already open DatabaseModel() will return an instance of it
-    DatabaseModel userPrefs = DatabaseModel();
+    DatabaseModel();
 
     return MaterialApp(
       title: 'Fitness App', //Temp name, to be changed later
       theme: ThemeManager.getThemeData(),
-      home: AppLaunch(), //Launch the app
+      home: _AppLaunch(), //Launch the app
     );
   }
 }
 
-class AppLaunch extends StatefulWidget //Make the AppLaunch a stateful widget
+class _AppLaunch extends StatefulWidget //Make the AppLaunch a stateful widget
 {
   @override
   _AppLaunchState createState() => _AppLaunchState();
 }
 
-final storage = FlutterSecureStorage(); //Session token storage location
+const storage = FlutterSecureStorage(); //Session token storage location
 
 //AppLaunch Handles user session status (Has session token or no session token).
 //If the session token exists, bring the user to home screen (using Home Navigator)
 //If the session token does not exist, bring them to login/make an account
-class _AppLaunchState extends State<AppLaunch>
+class _AppLaunchState extends State<_AppLaunch>
 {
   bool _isLoggedIn = false; //User not logged in by default
 
@@ -114,7 +115,7 @@ class HomeNavigator extends StatefulWidget
 {
   final VoidCallback logoutUser; //Calls back to logout definition to remove user token and set login status to false.
 
-  HomeNavigator({super.key, required this.logoutUser});
+  const HomeNavigator({super.key, required this.logoutUser});
 
   @override
   State<HomeNavigator> createState() => _HomeNavigatorState();
@@ -124,12 +125,12 @@ class _HomeNavigatorState extends State<HomeNavigator> {
   int _currentPageIndex = 0;
 
   //list of pages
-  late List<Widget> _pages = [
-    HomePage(),
-    NutritionPage(),
-    WorkoutPage(),
-    SocialPage(),
-    ProfilePage(logoutCallback: widget.logoutUser), //Passes logout functionailty to profile
+  late final List<Widget> _pages = [
+    const HomePage(),
+    const NutritionPage(),
+    const WorkoutPage(),
+    const SocialPage(),
+    ProfilePage(logoutCallback: widget.logoutUser), //Passes logout functionality to profile
   ];
 
   @override
