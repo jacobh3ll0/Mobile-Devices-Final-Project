e5e966ef1954a8a6db3f7f3e6695087d7200890f
Ryan Warrener <ryan.warrener@ontariotechu.net>
2024-11-15 14:55:28 -0500

    Added http request (in log)

--- lib/HomePage/home_page.dart
+++ lib/HomePage/home_page.dart
@@ -1,13 +1,102 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:md_final/global_widgets/build_bottom_app_bar.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+String calcTOD(int time)
+{
+  if (time >= 500 && time < 1159)
+    {
+      return "Good Morning User!";
+    }
+  else if (time >= 1200 && time < 1559)
+    {
+      return "Good Afternoon User!";
+    }
+  else if (time >= 1600 && time < 1959)
+    {
+      return "Good Evening User!";
+    }
+  else
+    {
+      return "Good Night User!";
+    }
 
+}
+
+String getTime()
+{
+  final dt = DateTime.now();
+  final dt24 = DateFormat('HH:mm').format(dt);
+  final hour = dt.hour.toString().padLeft(2,'0');
+  final min = dt.minute.toString().padLeft(2,'0');
+  final inttime = int.parse('$hour$min');
+  getQuote();
+  return calcTOD(inttime);
+
+}
+
+void getQuote() async
+{
+  final url = Uri.parse('https://zenquotes.io/api/today/');
+  final response = await http.get(url);
+  String quoteData = response.body.trim();
+  List<dynamic> parsedQuote = jsonDecode(quoteData);
+  Map<String, dynamic> quoteObject = parsedQuote[0];
+  log(quoteData);
+  log(quoteObject['q']);
+  log(quoteObject['a']);
+
+}
+
+
+
+class HomePage extends StatefulWidget
+{
+  @override
+  HomePageState createState() => HomePageState();
+}
+class HomePageState extends State<HomePage>
+{
+  // const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Page"), automaticallyImplyLeading: false),
+        appBar: AppBar(title: Text("Home Page"), automaticallyImplyLeading: false),
+        body:
+        Container(
+          decoration: BoxDecoration(
+            // image: DecorationImage(
+            //     image: AssetImage("testimage.PNG"),
+            //     fit: BoxFit.cover),
+          ),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(getTime()),
+                  Text("INSERT TOWN/CITY"),
+
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text('Hello'),
+                ],
+              )
+            ],
+          ),
+        )
     );
   }
 
